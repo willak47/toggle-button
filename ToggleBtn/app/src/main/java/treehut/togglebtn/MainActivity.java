a722f1c09c1b2b2ffe58e6c444cb7adba3ccb483
willak47 <gbannerman06@gmail.com>
2017-07-13 13:15:15 +0000

    new behavior

--- ToggleBtn/app/src/main/java/treehut/togglebtn/MainActivity.java
+++ ToggleBtn/app/src/main/java/treehut/togglebtn/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -36,4 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void onHitButtonClicked(View view) {
+        TextView textView =(TextView) findViewById(R.id.helloTextView);
+        textView.setVisibility(view.VISIBLE);
+    }
 }
